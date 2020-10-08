cf424cb3b009f6f5925223b534805a54f88efece
mihir879 <70084216+mihir879@users.noreply.github.com>
2020-10-08 11:54:06 +1100

    Update test_patch.groovy

--- test_patch.groovy
+++ test_patch.groovy
@@ -66,7 +66,7 @@ def wait_for_build_complete(String jobName, int build_number) {
 
 //CSV file read method
 @NonCPS
-def csvReader(String Filename)
+def csvReader(String FILENAME2)
 {
 	final CSV_HEADER_SERVERS = "Servers"
 	def inputFile = new File(Filename)
